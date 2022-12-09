91b3364a71051c77d972accc11a0d58f4b07394d
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2022-12-09 12:39:52 +0530

    changed views

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.apiOverview, name="api-overview"),
+    path('api/', views.apiOverview, name="api-overview"),
 ]
--- api/views.py
+++ api/views.py
@@ -2,17 +2,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import TaskSerializer
 from .models import Task
+"""
+Below Function going to display all the tasks store in the data base.
+"""
 
-"""
-API Overview
-"""
+
 @api_view(['GET'])
-def apiOverview(request):
-    api_urls = {
-        'List' : '/task-list/',
-        'Detail View' : '/task-detail/<str:pk>/',
-        'Create' : '/task-create/',
-        'Update' : '/task-update/<str:pk>/',
-        'Delete' : '/task-delete/<str:pk>/',
-    }
-    return Response(api_urls)
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
