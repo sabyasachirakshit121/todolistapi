e806f65437dd1517cb7a1aad6b25eb93e25113f5
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2022-12-09 12:23:36 +0530

    added model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    completed = models.BooleanField(default=False, blank=True, null=True)
+    due_date = models.DateField();
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
