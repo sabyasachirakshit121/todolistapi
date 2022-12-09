f32eae7abad6f635229e76d18a7a24f95751986d
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2022-12-09 12:46:02 +0530

    task detail url+api added

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('api/', views.apiOverview, name="api-overview"),
     path('task-list/', views.taskList, name="task-list"),
+    path('task-detail/<str:pk>/', views.taskDetail, name="task-Detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -24,3 +24,15 @@ def taskList(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+
+"""
+This Function going to display Detailed view of one perticuler task with the help of pk.
+"""
+
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=False)
+    return Response(serializer.data)
